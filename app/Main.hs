c5c0d90574941c37c58b3c5008a897b7c742ffd7
Nikita Dukin <kir.dukin@gmail.com>
2024-04-21 01:34:22 +0200

    feat(*): implement beautiful printer

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,11 @@ module Main where
 
 import Parser (Parser(..))
 import ParserTerm
+import Lib
+
 
 main :: IO ()
 main = case (getParserFunc parseFullTerm "(l x . (l h u . (l r . u)) x) x (y (z (k)))") of
     Left comment -> putStrLn comment
-    Right result -> putStrLn $ show result
+    Right (leftover, result) -> putStrLn $ beautyPrint result
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,2 +1,19 @@
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module Lib where
+
+import Data
+
+
+beautyPrint' :: T -> String
+beautyPrint' term = case term of
+    Var x -> x
+    Abstr args body -> "(\\" ++ (foldl (\p c -> p ++ c ++ " ") " " args) ++ "->" ++ beautyPrint body ++ ")"
+    App [one] -> beautyPrint' one
+    App terms -> "(" ++ (foldl (\p c -> p ++ (beautyPrint' c) ++ " ") " " terms) ++ ")"
+
+
+beautyPrint :: T -> String
+beautyPrint term = case term of
+    Var x -> show x
+    Abstr args body -> "\\" ++ (foldl (\p c -> p ++ c ++ " ") " " args) ++ "->" ++ beautyPrint' body
+    App terms -> (foldl (\p c -> p ++ (beautyPrint' c) ++ " ") " " terms)
